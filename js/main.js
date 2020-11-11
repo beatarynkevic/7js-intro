70a9304d2a91cbf054136824f86569d8d218421f
beatarynkevic <beatainn@gmail.com>
2020-11-11 15:51:38 +0200

    More code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,69 @@
+// spausdiname sveikaji skaiciu;
+console.log(321);
+
+// desimtainis
 console.log(3.14);
+
+// tekstas
 console.log("Labas");
+
+// KINTAMIEJI-VARIABLES
+
+const pirmasSkaicius = 2;
+const antrasSkaicius = 3;
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log("Pirmas skaicius turetu buti 2.");
+console.log(pirmasSkaicius);
+
+console.log("Antras skaicius turetu buti 3.");
+console.log(antrasSkaicius);
+
+console.log(suma);
+
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+const vardas  = "Beata";
+const metai = 25 ;
+
+const sakinys = vardas + ' yra ' + metai + ' metu amziaus';
+console.log(sakinys);
+
+//********************************************* */
+
+const cat = 'Burbonas';
+const maistas = 'skanukus';
+
+const sakinys2 = cat + ' megsta ' + maistas + ' 💕';
+console.log(sakinys2);
+
+const heroName = 'Chuck';
+const heroLastName = 'Norris';
+const birth = 1940;
+const currentYear = 2020;
+const age = currentYear - birth
+
+
+//Chuck Norris (80)
+ const sakinys3 = heroName + ' ' + heroLastName + ' ' + '(' + age + ')';
+ console.log(sakinys3);
+
+ console.clear();
+
+ let pinigine = 7;
+ const sausis = 100;
+ const vasaris = 200;
+ const kovas = 70;
+
+ //107
+ //307
+ //377
+
+ pinigine = pinigine + sausis;
+ console.log(pinigine);
+
+ pinigine = pinigine + vasaris;
+ console.log(pinigine);
+
+ pinigine = pinigine + kovas;
+ console.log(pinigine);
